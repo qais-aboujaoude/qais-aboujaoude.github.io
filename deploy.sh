c6a3ed0ccb646cee46ce4ba92fd3ee15bbcf8df9
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-05-21 15:12:37 +0300

    asdf asdfasdf

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 echo $BRANCH
 if [ "$BRANCH" != "master" ]
 then
-  git add . && git commit -m $MESSAGE && \
+  git add . && git commit -m "$MESSAGE" && \
   git checkout master && \
   git merge dev && \
   npm run generate && \
