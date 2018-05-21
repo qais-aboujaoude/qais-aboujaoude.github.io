b4c77a1198458dc83f7f7a52964f614a88ad8eb4
Qais Issa Aboujaoude <q.i.aboujaoude@gmail.com>
2018-05-21 11:39:52 +0300

    Add base route for github pages and conditional to build locally

---
+++ nuxt.config.js
@@ -0,0 +1,58 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/qais-aboujaoude.github.io/'
+  }
+} : {}
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'qais-aboujaoude',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'my website zoho!' }
+    ]
+    // ,
+    // link: [
+    //   { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+    //   { rel: 'stylesheet', href: 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' },
+    // ]
+  },
+  
+  router: {
+    base: '/qais-aboujaoude.github.io/'
+  }
+  // css: [
+  //   'assets/main.css'
+  // ],
+
+  // modules: ['@nuxtjs/bulma'],
+
+  // plugins: ['~/plugins/global.js'],
+  /*
+  ** Customize the progress bar color
+  */
+  // loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  // build: {
+    /*
+    ** Run ESLint on save
+    */
+  //   extend (config, { isDev, isClient }) {
+  //     if (isDev && isClient) {
+  //       config.module.rules.push({
+  //         enforce: 'pre',
+  //         test: /\.(js|vue)$/,
+  //         loader: 'eslint-loader',
+  //         exclude: /(node_modules)/
+  //       })
+  //     }
+  //   }
+  // }
+
+}
